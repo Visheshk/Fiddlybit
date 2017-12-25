106d159f4686a5fc5e973deae5dbaf11be31bf62
Vishesh Kumar <vishesh@visheshk.net>
2017-12-25 16:32:09 -0600

    Things work! - keypresses speeds up, and release works on sonic pi
    TODOS:
    Need to add bootstrap,
    change keypress to buttons

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var OSC = require('osc-js');
 var port = process.env.PORT || 3000;
+var Worker = require('webworker-threads').Worker;
+
 // gibbs = require('./gibber.audio.lib.min.js')
 
   const osc = new OSC({
@@ -18,6 +20,64 @@ app.get('/', function(req, res){
 
 });
 
+var worker = new Worker(function(){
+  postMessage("I'm working before postMessage('ali').");
+  this.onmessage = function(event) {
+    postMessage('Hi ' + event.data);
+    self.close();
+  };
+});
+worker.onmessage = function(event) {
+  console.log("Worker said : " + event.data);
+};
+worker.postMessage('ali');
+
+
+playing = 0;
+lastNotePlay = Date.now();
+notePlaying = 0;
+release = 1;
+noteArr = [[1, 60], [1, 60], [1, 67], [1, 67], [1, 69], [1, 69], [1, 67], [1, 20], [1, 65], [1, 65], [1, 64], [1, 64], [1, 62], [1, 62], [2, 60]]
+
+ticklength = 500;
+
+resumePlaying = function () {
+  if (checkPlayState() == 0){
+    playing = 1;
+    console.log(lastNotePlay);
+    lastNotePlay = Date.now();
+    playMusic();
+  }
+}
+
+stopPlaying = function () {
+  playing = 0;
+  console.log("stopping play");
+}
+
+checkPlayState = function () {
+  return(playing);
+}
+
+playMusic = function () {
+  playing = checkPlayState();
+  if (playing == 1) {
+    nowtime = Date.now();
+    console.log("playing + " + Date.now());
+    const message = new OSC.Message('/play_this', parseInt(noteArr[notePlaying][1]), parseInt(noteArr[notePlaying][0]));
+    osc.send(message);
+    notePlaying = (notePlaying + 1) % noteArr.length;
+    // if (nowtime - lastNotePlay > 800) {
+    //   lastNotePlay = nowtime;-
+    //   console.log("playing");
+    // }
+    setTimeout(playMusic, ticklength);
+  }
+  
+    // playMusic();
+}
+
+
 io.on('connection', function(socket){
   console.log('a user connected');
   console.log(socket.id);
@@ -28,9 +88,40 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
+    console.log(release);
     if (msg == "60" || msg == "70" || msg == "80"){
-      const message = new OSC.Message('/play_this', parseInt(msg));
+      const message = new OSC.Message('/play_this', parseInt(msg), release);
       osc.send(message);
+    }
+  });
+
+  socket.on('play', function (state) {
+    if (state == 1) {
+      resumePlaying();
+    }
+    if (state == 0) {
+      console.log("stop call");
+      stopPlaying();
+    }
+  });
+
+  socket.on('releaseChange', function (dir) {
+    release = release + (dir * 0.2);
+    if (release < 0) {
+      release = 0.2
+    }
+    else if (release > 10) {
+      release = 1;
+    }
+  });
+
+  socket.on('speedChange', function (dir) {
+    ticklength = ticklength + (dir * 150);
+    if (ticklength > 2000) {
+      ticklength = 250;
+    }
+    else if (release < 100) {
+      release = 2000;
     }
   });
 
