3e909989255b9cbb02de3506e6d0796b77a73a31
Vishesh Kumar <vishesh@visheshk.net>
2018-07-18 14:02:38 -0500

    Beginning to integrate two.js

--- sonic-pi/index.js
+++ sonic-pi/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var Two = require('two.js');
 // var OSC = require('osc-js');
 // var Tone = require('tone');
 var port = process.env.PORT || 18010;
